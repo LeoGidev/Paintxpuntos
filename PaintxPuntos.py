64232be7dd5d597e1b2b49b935e5bd0d9146a819
LeoGidev <leogimenez.dev@gmail.com>
2023-12-06 18:20:08 -0300

    se agrega bucle principal

--- PaintxPuntos.py
+++ PaintxPuntos.py
@@ -30,3 +30,26 @@ image = np.zeros((500, 500, 3), dtype=np.uint8)
 # Crear una ventana y asociar la función de manejo del mouse
 cv2.namedWindow("Arrastrar Vértices")
 cv2.setMouseCallback("Arrastrar Vértices", mouse_handler)
+
+# Bucle principal
+while True:
+    # Copiar la imagen para evitar sobrescribir los vértices
+    canvas = image.copy()
+
+    # Dibujar los vértices del polígono
+    for vertex in vertices:
+        cv2.circle(canvas, vertex, 5, (0, 0, 255), -1)
+
+    # Dibujar el polígono con líneas entre los vértices
+    if len(vertices) > 1:
+        cv2.polylines(canvas, [np.array(vertices)], False, (0, 255, 0), 2)
+
+    # Mostrar la imagen con los vértices y el polígono
+    cv2.imshow("Arrastrar Vértices", canvas)
+
+    # Esperar la tecla 'q' para salir del bucle
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Cerrar todas las ventanas
+cv2.destroyAllWindows()
