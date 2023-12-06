f3e15bedbb01fd15b0d6e48ffb42a471e920e936
LeoGidev <leogimenez.dev@gmail.com>
2023-12-06 18:17:55 -0300

    se agrega funcion

--- PaintxPuntos.py
+++ PaintxPuntos.py
@@ -5,3 +5,22 @@ import numpy as np
 dragging = False
 selected_vertex = None
 vertices = []
+
+# Función para manejar el evento del mouse
+def mouse_handler(event, x, y, flags, param):
+    global dragging, selected_vertex, vertices
+
+    if event == cv2.EVENT_LBUTTONDOWN:
+        # Agregar un nuevo vértice
+        vertices.append((x, y))
+
+    elif event == cv2.EVENT_LBUTTONUP:
+        # Detener el arrastre del vértice
+        dragging = False
+        selected_vertex = None
+
+    elif event == cv2.EVENT_MOUSEMOVE:
+        # Arrastrar el último vértice agregado
+        if len(vertices) > 0:
+            vertices[-1] = (x, y)
+
